e16d845b21a4e7d40b4c92217efdb992f8424b9e
Romain F. T <rrroschan@gmail.com>
2018-11-22 11:53:21 +0100

    Initial commit n°2

---
+++ src/main/java/fr/univ_nantes/slightstone_server/App.java
@@ -0,0 +1,13 @@
+package fr.univ_nantes.slightstone_server;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
