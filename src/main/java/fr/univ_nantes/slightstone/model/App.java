2b99ff4dd1d54295296107ac5d382fb078df3a23
Lastshot97 <juju7397@gmail.com>
2019-01-10 01:09:44 +0100

    Mise a jour methode pour peupler la bdd

--- src/main/java/fr/univ_nantes/slightstone/model/App.java
+++ src/main/java/fr/univ_nantes/slightstone/model/App.java
@@ -6,25 +6,24 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
-import static org.mockito.Mockito.mock;
+import java.util.Collections;
 
 @ComponentScan({"fr.univ_nantes.slightstone.model","fr.univ_nantes.slightstone.server"})
 @SpringBootApplication
 public class App {
 	
 	public static void main(String[] args) {
-		SpringApplication.run(App.class);
+		SpringApplication app = new SpringApplication(App.class);
+		app.setDefaultProperties(Collections.singletonMap("server.port", "8080"));
+		app.run(args);
 	}
-	/*
+	
 	@Bean
 	public CommandLineRunner initialisationBDD(HerosRepository hRepository, ActionsRepository aRepository, CartesRepository cRepository) {
 		return (args) -> {
-			//Initialisation d'un jeu fictif
-			Jeu jeu = mock(Jeu.class);
-			
 			//Action spéciale du Mage
 			DescripteurSort actionSpecialeMage = new DescripteurSort("Boule de feu", "Boule de feu, infligeant un point de dégats à un adversaire (serviteur ou héros)", "bouleDeFeu.png", ClasseHeros.MAGE, 2);
-			ActionAttaque bouleDeFeu = new ActionAttaque(jeu, TypeCible.UN_ADVERSAIRE, 1);
+			ActionAttaque bouleDeFeu = new ActionAttaque(TypeCible.UN_ADVERSAIRE, 1);
 			actionSpecialeMage.ajouterAction(bouleDeFeu);
 			aRepository.save(bouleDeFeu);
 			cRepository.save(actionSpecialeMage);
@@ -36,7 +35,7 @@ public class App {
 			//Action spéciale du Paladin
 			DescripteurSort actionSpecialePaladin = new DescripteurSort("Renfort", "Renfort, invoquant un serviteur \"recrue de la Main d'argent\" 1/1", "recrueMainArgent.png", ClasseHeros.PALADIN, 2);
 			DescripteurServiteur serviteurRenfort = new DescripteurServiteur("Recrue de la main d'argent", "Serviteur 1/1", "recrueMainArgent.png", ClasseHeros.PALADIN, 0, 1, 1, false, false, false, false);
-			ActionInvocation invocationRenfort = new ActionInvocation(jeu, serviteurRenfort);
+			ActionInvocation invocationRenfort = new ActionInvocation(serviteurRenfort);
 			actionSpecialePaladin.ajouterAction(invocationRenfort);
 			cRepository.save(serviteurRenfort);
 			aRepository.save(invocationRenfort);
@@ -48,7 +47,7 @@ public class App {
 			
 			//Action spéciale du Guerrier
 			DescripteurSort actionSpecialeGuerrier = new DescripteurSort("Armure", "Armure, lui conférant 2 points d'armure", "armure.png", ClasseHeros.GUERRIER, 2);
-			ActionBoostArmure armure = new ActionBoostArmure(jeu, 2);
+			ActionBoostArmure armure = new ActionBoostArmure(2);
 			actionSpecialeGuerrier.ajouterAction(armure);
 			aRepository.save(armure);
 			cRepository.save(actionSpecialeGuerrier);
@@ -76,7 +75,7 @@ public class App {
 			//Cartes spécifiques au mage
 			DescripteurSort imageMiroir = new DescripteurSort("Image miroir", "Sort de 1 point de mana, invoque deux serviteurs 0/2 avec provocation", "imageMiroir.png", ClasseHeros.MAGE, 1);
 			DescripteurServiteur serviteurImageMiroir = new DescripteurServiteur("Serviteur miroir", "Serviteur 0/2", "serviteurMiroir.png", ClasseHeros.MAGE, 0, 0, 2, true, false, false, false);
-			ActionInvocation invocationImageMiroir = new ActionInvocation(jeu, serviteurImageMiroir);
+			ActionInvocation invocationImageMiroir = new ActionInvocation(serviteurImageMiroir);
 			imageMiroir.ajouterAction(invocationImageMiroir);
 			imageMiroir.ajouterAction(invocationImageMiroir);
 			cRepository.save(serviteurImageMiroir);
@@ -84,13 +83,13 @@ public class App {
 			cRepository.save(imageMiroir);
 			
 			DescripteurSort explosionDesArcanes = new DescripteurSort("Explosion des arcanes", "Sort de 2 points de mana, inflige 1 point de dégats à tous les serviteurs adverses", "explosionDesArcanes.png", ClasseHeros.MAGE, 2);
-			ActionAttaque attaqueExplosionDesArcanes = new ActionAttaque(jeu, TypeCible.TOUS_SERVITEURS_ADVERSES, 1);
+			ActionAttaque attaqueExplosionDesArcanes = new ActionAttaque(TypeCible.TOUS_SERVITEURS_ADVERSES, 1);
 			explosionDesArcanes.ajouterAction(attaqueExplosionDesArcanes);
 			aRepository.save(attaqueExplosionDesArcanes);
 			cRepository.save(explosionDesArcanes);
 			
 			DescripteurSort metamorphose = new DescripteurSort("Métamorphose", "Sort de 4 points de mana, transforme un serviteur en serviteur 1/1 sans effet spécial", "metamorphose.png", ClasseHeros.MAGE, 4);
-			ActionMouton metamorphoseMouton = new ActionMouton(jeu);
+			ActionMouton metamorphoseMouton = new ActionMouton();
 			metamorphose.ajouterAction(metamorphoseMouton);
 			aRepository.save(metamorphoseMouton);
 			cRepository.save(metamorphose);
@@ -100,20 +99,20 @@ public class App {
 			cRepository.save(championFrisselame);
 			
 			DescripteurSort benedictionDePuissance = new DescripteurSort("Bénédiction de puissance", "Sort de 1 point de mana, confère +3 d'attaque à un serviteur", "benedictionDePuissance.png", ClasseHeros.PALADIN, 1);
-			ActionBoostDegats buffBenedictionDePuissance = new ActionBoostDegats(jeu, 3);
+			ActionBoostDegats buffBenedictionDePuissance = new ActionBoostDegats(3);
 			benedictionDePuissance.ajouterAction(buffBenedictionDePuissance);
 			aRepository.save(buffBenedictionDePuissance);
 			cRepository.save(benedictionDePuissance);
 			
 			DescripteurSort consecration = new DescripteurSort("Consécration", "Sort de 4 points de mana, inflige 2 points de dégâts à tous les adversaires", "consecration.png", ClasseHeros.PALADIN, 4);
-			ActionAttaque attaqueConsecration = new ActionAttaque(jeu, TypeCible.TOUS_ADVERSAIRES, 2);
+			ActionAttaque attaqueConsecration = new ActionAttaque(TypeCible.TOUS_ADVERSAIRES, 2);
 			consecration.ajouterAction(attaqueConsecration);
 			aRepository.save(attaqueConsecration);
 			cRepository.save(consecration);
 			
 			//Cartes spécifiques au guerrier
 			DescripteurSort tourbillon = new DescripteurSort("Tourbillon", "Sort de 1 point de mana, inflige 1 point de dégats à tous les serviteurs (y compris les vôtres)", "tourbillon.png", ClasseHeros.GUERRIER, 1);
-			ActionAttaque attaqueTourbillon = new ActionAttaque(jeu, TypeCible.TOUS_SERVITEURS, 1);
+			ActionAttaque attaqueTourbillon = new ActionAttaque(TypeCible.TOUS_SERVITEURS, 1);
 			tourbillon.ajouterAction(attaqueTourbillon);
 			aRepository.save(attaqueTourbillon);
 			cRepository.save(tourbillon);
@@ -122,8 +121,8 @@ public class App {
 			cRepository.save(avocatCommisOffice);
 			
 			DescripteurSort maitriseDuBlocage = new DescripteurSort("Maitrise du blocage", "Sort de 3 points de mana, +5 d'armure et place une carte aléatoire de la pioche dans votre main", "maitriseDuBlocage.png", ClasseHeros.GUERRIER, 3);
-			ActionBoostArmure buffArmureMaitriseDuBlocage = new ActionBoostArmure(jeu, 5);
-			ActionPioche piocheMaitriseDuBlocage = new ActionPioche(jeu);
+			ActionBoostArmure buffArmureMaitriseDuBlocage = new ActionBoostArmure(5);
+			ActionPioche piocheMaitriseDuBlocage = new ActionPioche();
 			maitriseDuBlocage.ajouterAction(buffArmureMaitriseDuBlocage);
 			maitriseDuBlocage.ajouterAction(piocheMaitriseDuBlocage);
 			aRepository.save(buffArmureMaitriseDuBlocage);
@@ -131,7 +130,7 @@ public class App {
 			cRepository.save(maitriseDuBlocage);
 		};
 	}
-	*/
+	
 	/*
 	@Bean
 	public CommandLineRunner demo() {
